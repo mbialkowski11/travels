392abeeda3f77c5a83cd8f8105fbbb3303b33bcd
Michał Białkowski <michal.bialkowski11@gmail.com>
2023-10-15 13:14:56 +0200

    Add sign up test

---
+++ src/test/java/SignUpSearch.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class SignUpSearch {
+
+    @Test
+    public void signUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+
+        String lastname = "Testowy";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "tester" + randomNumber + "@tester.pl";
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.name("firstname")).sendKeys("Bartek");
+        driver.findElement(By.name("lastname")).sendKeys(lastname);
+        driver.findElement(By.name("phone")).sendKeys("111222333");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains(lastname));
+        Assert.assertEquals(heading.getText(),"Hi, Bartek Testowy");
+
+
+        //driver.quit();
+
+
+    }
+
+}
