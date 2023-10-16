2aab821d22171852a14bda7ec221e1223df9e4ef
Michał Białkowski <michal.bialkowski11@gmail.com>
2023-10-16 13:56:23 +0200

    Add sign up empty form test

---
+++ src/test/java/SignUpSearchHomework.java
@@ -0,0 +1,77 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class SignUpSearchHomework {
+
+    @Test
+    public void signUpWithEmptyForm() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']//p")).stream()
+                                                                                .map(WebElement::getText)
+                                                                                .collect(Collectors.toList());
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(errors.contains("The Email field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The First name field is required."));
+        softAssert.assertTrue(errors.contains("The Last Name field is required."));
+        softAssert.assertAll();
+
+
+        driver.quit();
+
+
+    }
+
+
+    @Test
+    public void signUpWithWrongEmail() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        String lastname = "Testowy";
+        driver.findElement(By.name("firstname")).sendKeys("Bartek");
+        driver.findElement(By.name("lastname")).sendKeys(lastname);
+        driver.findElement(By.name("phone")).sendKeys("111111111");
+        driver.findElement(By.name("email")).sendKeys("tester.pl");
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebElement warning = driver.findElement(By.xpath("//div[@class='alert alert-danger']//p"));
+        SoftAssert softAssert = new SoftAssert();
+        //softAssert.assertTrue(warning.conta);
+
+       // driver.quit();
+
+    }
+}
