7eb5aa7e923f57a869c6a766a78bac87c31bbdfd
Michał Białkowski <michal.bialkowski11@gmail.com>
2023-10-24 14:21:58 +0200

    Remove user class

--- src/test/java/pl/seleniumdemo/pages/SignUpPage.java
+++ src/test/java/pl/seleniumdemo/pages/SignUpPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import pl.seleniumdemo.model.User;
 
 import java.util.List;
 import java.util.stream.Collectors;
