090e53032bbb5e190d632f176fc6e1bf68124bd6
Michał Białkowski <michal.bialkowski11@gmail.com>
2023-11-15 12:03:17 +0100

    Update code with Dataprovider Add test with data from Excel

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -1,10 +1,13 @@
 package pl.seleniumdemo.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.pages.ResultsPage;
+import pl.seleniumdemo.utils.ExcelReader;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HotelSearchTest extends BaseTest {
@@ -30,6 +33,30 @@ public class HotelSearchTest extends BaseTest {
         Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
 
 
+    }
+
+    @Test(dataProvider = "data")
+    public void searchHotelTestWithDataProvider(String city, String hotel) {
+
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setCity(city);
+        hotelSearchPage.setDates("27/04/2021", "29/04/2021");
+        hotelSearchPage.setTravellers(1, 2);
+        hotelSearchPage.performSearch();
+
+
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
+
+
+        Assert.assertEquals(hotelNames.get(0), hotel);
+
+
+
+    }
+    @DataProvider
+    public Object[][] data() throws IOException {
+        return ExcelReader.readExcel("testData.xlsx");
     }
 
 }
